69abc8875fb05f7acaa773e25f924f70878a3bc2
muhammad <fajzievmuhammad09@gmail.com>
2025-10-13 15:41:09 +0500

    modified:   src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: ['http://localhost:3000'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  });
-
   const port = process.env.PORT || 8000;
   await app.listen(port, () => console.log(`Server running on port ${port}`));
 }
