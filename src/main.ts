5ea92e2e770a2d33c7f21d117b291d721ce60539
muhammad <fajzievmuhammad09@gmail.com>
2025-10-13 15:22:23 +0500

    modified:   src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000'], // frontend URL, productionda sizning domain
+    origin: ['http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
