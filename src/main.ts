a531e693e75b612600d4cb60c01212e341980ec1
muhammad <fajzievmuhammad09@gmail.com>
2025-10-15 11:44:27 +0500

    modified:   src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 🔹 CORS ni yoqish (frontend bilan backendni bog‘lash uchun)
+  app.enableCors({
+    origin: [
+      'http://localhost:3000', // localda ishlaganda
+      'https://juniornest.vercel.app', // agar fronting Vercelda bo‘lsa shu manzilni yoz
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   const port = process.env.PORT || 8000;
-  await app.listen(port, () => console.log(`Server running on port ${port}`));
+  await app.listen(port, () => console.log(`✅ Server running on port ${port}`));
 }
+
 bootstrap();
