853c777ac4ccdbf730e6622f1dc5315cc691a7d6
muhammad <fajzievmuhammad09@gmail.com>
2025-11-10 16:39:15 +0500

    modified:   src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(cookieParser()); // 👈 cookie parser qo‘shildi
+
   app.enableCors({
     origin: [
-      'http://localhost:3000', 
+      'http://localhost:3000',
       'https://junioruz.vercel.app',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
+    credentials: true, // 👈 cookie yuborish uchun
   });
 
   const port = process.env.PORT || 8000;
